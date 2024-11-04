0b773451b22331bf459fc6e1a8b2f5136272c487
jasonMB12 <112223099+jasonmoyaB@users.noreply.github.com>
2024-11-04 17:55:54 -0600

    cards
    se actualizan las cartas

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
     <section class="cards">
         <div class="containers">
-            <div class="box"><h2>Nosotros</h2></div>
-            <div class="box"><h2>Tratamientos</h2></div>
-            <div class="box"><h2>Ubicanos</h2></div>
+            <div class="box"><h2>Nosotros</h2><br><p> Nos dedicamos a mejorar la calidad de vida de nuestros usuarios brindando acceso a medicamentos de alta calidad y asesoría en el manejo adecuado de sus tratamientos.</p></div>
+            <div class="box"><h2>Tratamientos</h2><br><p>Ofrecemos una amplia variedad de medicamentos para el tratamiento de enfermedades crónicas, agudas y preventivas, abarcando desde los más comunes hasta aquellos de especialidad.</p></div>
+            <div class="box"><h2>Ubicanos</h2><br><p>Visítanos en nuestras instalaciones para recibir atención personalizada y obtener los productos que necesitas. Estamos ubicados en una zona accesible, con opciones de transporte cercanas y estacionamiento disponible para mayor comodidad.</p></div>
         </div>
     </section>
 </main>
