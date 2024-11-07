60429d3ec362a4a22bfea98d304a268166bd5a6c
jasonMB12 <112223099+jasonmoyaB@users.noreply.github.com>
2024-11-06 19:38:06 -0600

    menu desplegale
    edito el menu a mandar a los servicios

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -8,19 +8,41 @@
     <link rel="stylesheet" href="./app/css/mainIndex.css">
 </head>
 <body>
-    <header class="header-prin">
-        <nav class="header-nav">
-            <a href="/" aria-label="Página de inicio" class="logo">
-                <img src="./app/img/MediTracksvg.svg" class="logo-header" alt="Logotipo de MediTrack">
-            </a>
-            <ul class="header-list">
-                <li><a href="">Inicio de sesion</a></li>
-                <li><a href="">Crear Cuenta</a></li>
-                <li><a href="">Servicios</a></li>
-                <li><a href="">Contacto</a></li>
-            </ul>
-        </nav>
-   
-    </header>
+<header class="header-prin">
+    <nav class="header-nav">
+        <a href="/" aria-label="Página de inicio" class="logo">
+            <img src="./app/img/MediTracksvg.svg" class="logo-header" alt="Logotipo de MediTrack">
+        </a>
+        <ul class="header-list">
+            <li><a href="">Inicio de sesión</a></li>
+            <li><a href="">Crear Cuenta</a></li>
+            <li class="navbar dropdown"> 
+                Servicios
+                <div class="dropdown-content">
+                    <div class="submenu">
+                        <a href="./app/components/functions/createAppointment.php">Crear cita</a>
+                        <div class="sub-submenu">
+                            <a href="./app/components/functions/showAppoin.php">Mostrar cita</a>
+                        </div>
+                    </div>
+                    <div class="submenu">
+                        <a href="./app/components/functions/createEmergency.php">Crear emergencia</a>
+                        <div class="sub-submenu">
+                            <a href="showEmergency">Mostrar emergencia</a>
+                        </div>
+                    </div>
+                    <div class="submenu">
+                        <a href="./app/components/functions/createMedicine.php">Crear medicina</a>
+                        <div class="sub-submenu">
+                            <a href="./app/components/functions/showMedications.php">Mostrar medicaciones</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+
     
     <body>
