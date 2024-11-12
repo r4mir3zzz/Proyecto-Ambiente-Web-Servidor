a06436b8a5c0300a5c6604f4e2d21037afbcd9f1
Luis Ramirez Acosta <123049540+r4mir3zzz@users.noreply.github.com>
2024-11-12 15:47:42 -0600

    Mejora mensaje

--- public/sendMessage.php
+++ public/sendMessage.php
@@ -11,6 +11,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("sss", $nombre, $email, $mensaje);
 
+    if ($stmt->execute()) {
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit; 
+    } else {
+        echo "Error al insertar el mensaje: " . $stmt->error;
+    }
+
     $stmt->close();
     $conn->close();
 }
