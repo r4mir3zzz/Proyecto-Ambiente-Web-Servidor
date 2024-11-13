da775e44d78a1d517376c44195aa1499cb919b73
Lucia <luciamora028@gmail.com>
2024-11-13 00:10:05 -0600

    Update signup.php

--- public/signup.php
+++ public/signup.php
@@ -1,31 +1,29 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear Cuenta</title>
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
+<?php
+include 'header.php';
+?>
+
+<main class="mainLogin">
     <div class="login-container">
-        <h2>Crear Cuenta en MediTrack</h2>
-        
-        <form action="#" method="post">
-            <label for="nombre">Nombre</label>
-            <input type="text" id="nombre" name="nombre" required>
+        <h2 class="welcomeMessage">Crear Cuenta en MediTrack</h2>
+        <?php
+            include "../config/conexion.php";
+            include "./register.php";
+        ?>
+        <form action="" method="post" class="formLogin">
+            <label for="nombre" class="labelLogin">Nombre</label>
+            <input type="text" id="nombre" name="nombre" class="inputLogin" required>
 
-            <label for="correo">Correo electrónico</label>
-            <input type="text" id="correo" name="correo" required>
+            <label for="correo" class="labelLogin">Correo electrónico</label>
+            <input type="text" id="correo" name="correo" class="inputLogin" required>
             
-            <label for="password">Contraseña</label>
-            <input type="password" id="password" name="password" required>
+            <label for="password" class="labelLogin">Contraseña</label>
+            <input type="password" id="password" name="password" class="inputLogin" required>
 
-            <label for="confirm-password">Confirmar Contraseña</label>
-            <input type="password" id="confirm-password" name="confirm-password" required>
+            <label for="confirm-password" class="labelLogin">Confirmar Contraseña</label>
+            <input type="password" id="confirm-password" name="confirm-password" class="inputLogin" required>
             
-            <button type="submit">Crear Cuenta</button>
+            <input type="submit" value="Crear Cuenta" class="btningresar" name="btncrear">
             <p class="signup-link">¿Ya tienes cuenta? <a href="#">Inicia Sesión aquí</a></p>
         </form>
     </div>
-</body>
-</html>
+</main>
