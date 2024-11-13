89f8f4723e2f3e56b4031a5c69458d29fe53d7ed
Geovanny Arias <geoas121cr@gmail.com>
2024-11-12 22:55:53 -0600

    Actualizar estilos y campos en el formulario de creación de medicamentos

--- public/createMedicine.php
+++ public/createMedicine.php
@@ -3,31 +3,34 @@ include 'header.php';
 ?>
 
 <main>
-<link rel="stylesheet" href="../assets/css/createMedicine.css">
+    <link rel="stylesheet" href="../assets/css/createMedicine.css">
     <div class="container">
-        <h1 class="tileForm">Formulario de Medicamentos <p>Rellene el formulario de inscripción</p></h1>
+        <h1 class="tileForm">Formulario de Medicamentos <p>Rellene el formulario de inscripción</p>
+        </h1>
         <form action="saveMedicine.php" method="post">
             <div class="nombreMedicamento">
                 <h2>Nombre del Medicamento</h2>
-                <input class="input-nombre" type="text" name="name" id="name" required>
+                <input class="input-nombre" type="text" name="nombre" id="nombre" required>
             </div>
-            
             <div class="descripcionMedicamento">
                 <h2>Descripción</h2>
-                <textarea class="input-descripcion" name="description" id="description" required></textarea>
+                <textarea class="input-descripcion" name="descripcion" id="descripcion" required></textarea>
             </div>
 
-            <div class="precioMedicamento">
-                <h2>Precio</h2>
-                <input class="input-precio" type="number" name="price" id="price" step="1" required>
+            <div class="dosisMedicamento">
+                <h2>Dosis</h2>
+                <input class="input-dosis" type="text" name="dosis" id="dosis" required>
             </div>
 
+            <div class="frecuenciaMedicamento">
+                <h2>Frecuencia</h2>
+                <input class="input-frecuencia" type="text" name="frecuencia" id="frecuencia" required>
+            </div>
             <div class="cantidadMedicamento">
                 <h2>Cantidad</h2>
-                <input class="input-cantidad" min="1" type="number" name="quantity" id="quantity" required>
+                <input class="input-cantidad" type="number" name="cantidad" id="cantidad" min="0" required>
             </div>
-
-            <input class="botonEnviar" type="button" value="Crear" id="btnsubmit">
+            <input class="botonCrear" type="submit" value="Crear">
         </form>
     </div>
     <br><br>
